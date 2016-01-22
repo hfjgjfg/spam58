189a4af8e8136b64f4008e3b3b7dbaa74372ee45
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-01-22 11:10:33 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {119989724},
+    sudo_users = {150575718},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
